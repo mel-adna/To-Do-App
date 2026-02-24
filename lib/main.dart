d6e403973b2defe87bbb1f7bcef90857e232b067
mohamed eladnani <mohamedeladnani0@gmail.com>
2026-02-24 13:45:05 +0000

    add flutter ci

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/features/auth/presentation/views/login_view.dart';
-import 'package:to_do_app/features/splash/presentation/views/splash_views.dart';
 
 void main() {
   runApp(const ToDoList());
